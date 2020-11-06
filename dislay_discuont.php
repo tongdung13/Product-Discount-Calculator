64fdae9b35ca20281d82bdb30a8a417217a292d4
tongdung13 <tongvanthienvu@gmail.com>
2020-11-06 23:30:18 +0700

    ok

--- dislay_discuont.php
+++ dislay_discuont.php
@@ -5,7 +5,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $discount = $_POST["discount"];
 }
 echo "Product:" .$product;
-$discountamount = ($list * $discount) * 0.1;
+$discountamount = (($list * $discount) * 0.1) /10;
 echo "<br><br>Discount Amount: " .$discountamount;
 $discoountprice = $list - $discountamount;
 echo "<br><br>Discount Price: " .$discoountprice;
